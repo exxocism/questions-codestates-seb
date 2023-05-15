9000fb5c585259f658f17b15cf12eccf866ddeed
Minwoo Choi <minwoo.choi@tapjoy.com>
2023-05-15 23:48:19 +0900

    feat: routing scaffolding

---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import App from './App';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: 'about',
+    element: <div>About</div>,
+  },
+]);
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { RouterProvider } from 'react-router';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import { router } from './Router';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
