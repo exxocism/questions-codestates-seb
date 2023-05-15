c28b562bcc0e5bb8895a7c9fa06152e9526b5c27
Minwoo Choi <minwoo.choi@tapjoy.com>
2023-05-16 00:41:36 +0900

    feat: add screen assets

---
+++ src/components/Screen.tsx
@@ -0,0 +1,10 @@
+import { styled } from 'styled-components';
+
+export const Main = styled.main`
+  display: flex;
+  justify-content: center;
+`;
+
+export const CenteredScreen = styled.div`
+  max-width: 1280px;
+`;
